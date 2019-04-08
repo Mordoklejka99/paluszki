45bd2157f6bcf97fb0f2ef465896bedf1f6c90d0
Mordoklejka <marcing305@wp.pl>
2019-04-08 15:42:12 +0200

    Initialize file

--- Initialize.java
+++ Initialize.java
@@ -1,5 +1,3 @@
-package pl.replayGames.paluszki;
-
 import java.util.Scanner;
 
 public class Initialize {
@@ -12,15 +10,13 @@ public class Initialize {
 			System.out.println("How many players? (from 2 to 6)");
 			players = in.nextInt();
 		}
-		while(!(players>1 && players<7));
+		while(!(players > 1 && players < 7));
 		do {
 			System.out.println("How many fingers at the beggining? (from 1 to 4)");
 			fingers = in.nextInt();
 		}
-		while(!(fingers>0 && fingers<5));
+		while(!(fingers > 0 && fingers < 5));
 		Game gra = new Game(fingers, players);
 		gra.run();
-		//Thread th = new Thread(gra);
-		//th.start();
 	}
 }
